11af91600bc292bc71a85f4c06235c2df98c1e9d
rfqhmmwn <rifqihimmawan@gmail.com>
2026-01-06 14:36:32 +0700

    function get galery

---
+++ inc/function_galeri.php
@@ -0,0 +1,14 @@
+<?php
+function get_galery(){
+        global $db;
+        $get = $db->get('`galeri`');
+        return $get;
+    }
+
+    function get_galery_by_id($id) {
+        global $db;
+        $db->where ("id", $id);
+        $get = $db->getOne ("`galeri`");
+        return $get;
+    }
+?>
